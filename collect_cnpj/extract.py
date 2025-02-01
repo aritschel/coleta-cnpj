369793bdf971634e81fdf9808e52c856437bbf03
aritschel <52209458+aritschel@users.noreply.github.com>
2025-02-01 17:37:42 +0000

    refactor: extract

--- collect_cnpj/extract.py
+++ collect_cnpj/extract.py
@@ -5,13 +5,8 @@ import os
 from utils.helpers import set_url, set_file_name
 
 
-data_date = "2024-11/"
-
-data_base = "Socios9"
-data_type = ".zip"
-file_name = set_file_name(data_base, data_type)
-
-def download_file(data_date: str, file_name: str):
+def download_file(data_date: str, file_name: str) -> None:
+    """Downloads a file from a generated URL."""
     url = set_url(data_date, file_name)
     response = requests.get(url, stream=True)
 
@@ -19,28 +14,32 @@ def download_file(data_date: str, file_name: str):
         with open(file_name, "wb") as file:
             file.write(response.content)
 
-def extract_files(file_name: str, data_base: str):
-    zip = ZipFile(file_name)
-    zip.extractall(data_base)
-    zip.close()
+def extract_files(file_name: str, data_base: str) -> None:
+    """Extracts files from a ZIP archive to a specific directory."""
+    with ZipFile(file_name, 'r') as zip_ref:
+        zip_ref.extractall(data_base)
 
-
-def change_file_name(data_base: str):
-    diretorio_original = os.getcwd()
+def rename_extracted_file(data_base: str) -> None:
+    """Renames the file found in the directory to a new name."""
+    original_directory = os.getcwd()
     data_type=".csv"
-    file_name = set_file_name(data_base, data_type)
+    new_file_name = set_file_name(data_base, data_type)
     try:
         os.chdir(data_base)
-        for arquivo in os.listdir():
-            os.rename(arquivo, file_name)
-            break
+        for file in os.listdir():
+            os.rename(file, new_file_name)
     finally:
-        os.chdir(diretorio_original)
+        os.chdir(original_directory)
 
-# print(url)
-#extract_files(data_type, data_base, file_name)
+def main() -> None:
+    """Orchestrates the process of downloading, extracting, and renaming files."""
+    data_date = "2024-11/"
+    data_base = "Socios9"
+    data_type = ".zip"
+    file_name = set_file_name(data_base, data_type)
+    download_file(data_date, file_name)
+    extract_files(file_name, data_base)
+    rename_extracted_file(data_base)
 
-change_file_name(data_base)
-# diretorio_original = os.getcwd()
-
-# change_file_name()
+if __name__ == "__main__":
+    main()
--- collect_cnpj/utils/helpers.py
+++ collect_cnpj/utils/helpers.py
@@ -1,6 +1,6 @@
 def set_file_name(data_base: str, data_type: str) -> str:
     return data_base+data_type
 
-def set_url(endpoint: str, data_date: str, file_name: str) -> str:
+def set_url(data_date: str, file_name: str) -> str:
     endpoint = "https://arquivos.receitafederal.gov.br/dados/cnpj/dados_abertos_cnpj/"
     return endpoint+data_date+file_name
